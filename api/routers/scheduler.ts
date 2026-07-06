ead4aee32a5c4f358526d2faa76834bd2465b9df
onxos <mshaheen5@hotmail.com>
2026-07-06 18:44:14 +0300

    feat: update scheduler router

--- api/routers/scheduler.ts
+++ api/routers/scheduler.ts
@@ -3,5 +3,9 @@ import { rhythms } from "../lib/datasets";
 
 export const schedulerRouter = createTRPCRouter({
   rhythms: publicProcedure.query(() => rhythms),
-  status: publicProcedure.query(() => ({ active: true, count: rhythms.length }))
+  status: publicProcedure.query(() => ({
+    active: true,
+    count: rhythms.length,
+    rhythms: rhythms.map((r, i) => ({ name: r, phase: i + 1 }))
+  }))
 });
