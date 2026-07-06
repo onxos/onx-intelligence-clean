a61bbbed15da69924413d63994e084203a566030
onxos <mshaheen5@hotmail.com>
2026-07-06 18:44:12 +0300

    feat: update App.tsx routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Landing } from "./pages/Landing";
+import { Home } from "./pages/Home";
 import { Ask } from "./pages/Ask";
 import { DashboardV2 } from "./pages/DashboardV2";
+import { Login } from "./pages/Login";
+import { NotFound } from "./pages/NotFound";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/ask" element={<Ask />} />
       <Route path="/dashboard" element={<DashboardV2 />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
